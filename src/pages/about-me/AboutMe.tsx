37f56192fd8febac6d571d0806375bfc403b708d
Jean Humblot <jhumblot1@gmail.com>
2021-07-05 21:10:28 +0900

    responsive modal

--- src/pages/about-me/AboutMe.tsx
+++ src/pages/about-me/AboutMe.tsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import { useDispatch, useSelector } from 'react-redux';
+import throttle from 'lodash.throttle';
 
 import './AboutMe.scss';
 import About from './components/About';
 import Skills from './components/Skills';
 import { onToggleAboutModal, selectIsAboutModalOpen } from '../../store/store';
 import useAnimation from '../../hooks/useAnimation';
+import useSize from '../../hooks/useSize';
 
 Modal.setAppElement('body');
 
 const AboutMe = () => {
   const [contentRef, setContentRef] = useState<HTMLDivElement|null>(null);
   const dispatch = useDispatch();
+  const [width] = useSize();
   const { gsap } = useAnimation();
   const isAboutModalOpen = useSelector(selectIsAboutModalOpen);
 
@@ -23,24 +26,28 @@ const AboutMe = () => {
       return;
     }
 
-    const tl = gsap.timeline({
-      defaults: {
-        ease: 'power1.in',
-        duration: 0.5,
-      },
-    });
+    const startAnimation = throttle(() => {
+      const tl = gsap.timeline({
+        defaults: {
+          ease: 'power1.in',
+          duration: 0.5,
+        },
+      });
 
-    if (window.innerWidth > 768) {
-      tl.from(contentRef.querySelector('.about'),
-        { y: '-100%', opacity: 0 }, 0);
-      tl.from(contentRef.querySelector('.skills'),
-        { y: '100%', opacity: 0 }, 0.2);
-    } else {
-      tl.from([contentRef.querySelector('.about'), contentRef.querySelector('.skills')],
-        { x: '-100%', opacity: 0 });
-    }
+      if (width > 768) {
+        tl.fromTo(contentRef.querySelector('.about'),
+          { y: '-100%', opacity: 0 }, { y: 0, opacity: 1 }, 0);
+        tl.fromTo(contentRef.querySelector('.skills'),
+          { y: '100%', opacity: 0 }, { y: 0, opacity: 1 }, 0.2);
+      } else {
+        tl.fromTo([contentRef.querySelector('.about'), contentRef.querySelector('.skills')],
+          { x: '-100%', opacity: 0 }, { x: 0, opacity: 1 });
+      }
+    }, 100);
+
+    startAnimation();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [contentRef]);
+  }, [contentRef, width]);
 
   return (
     <>
