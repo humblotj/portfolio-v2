cbedb5143f5d587fcf94a7eafd947350e94af4f7
Jean Humblot <jhumblot1@gmail.com>
2021-07-06 22:39:08 +0900

    scrolltotop use hook

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,22 +1,25 @@
 import { FunctionComponent, useEffect } from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { withRouter, RouteComponentProps, useLocation } from 'react-router-dom';
 
-const ScrollToTop: FunctionComponent<RouteComponentProps> = ({ history }) => {
+const ScrollToTop: FunctionComponent<RouteComponentProps> = () => {
+  const { pathname } = useLocation();
+
   useEffect(() => {
-    const unlisten = history.listen(() => {
-      window.scrollTo(0, 0);
-    });
     window.onpagehide = (event: PageTransitionEvent) => {
       if (!event.persisted) {
-        window.scrollTo(0, 0);
+        // window.scrollTo(0, 0);
+        // console.log(window.scrollTo(0, 0));
       }
     };
     return () => {
-      unlisten();
       window.onpagehide = null;
     };
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (null);
 };
