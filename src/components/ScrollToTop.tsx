6fa34dc1659bca5542a4a2508bbf4256edcb3778
Jean Humblot <jhumblot1@gmail.com>
2021-09-27 00:21:51 +0900

    Scroll to top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,21 +1,24 @@
 import { FunctionComponent, useEffect } from 'react';
-import { withRouter, RouteComponentProps, useLocation } from 'react-router-dom';
+import {
+  withRouter, RouteComponentProps, useLocation, useHistory,
+} from 'react-router-dom';
 
 const ScrollToTop: FunctionComponent<RouteComponentProps> = () => {
   const { pathname } = useLocation();
+  const history = useHistory();
 
   useEffect(() => {
     window.onpagehide = (event: PageTransitionEvent) => {
       if (!event.persisted) {
-        // window.scrollTo(0, 0);
-        // console.log(window.scrollTo(0, 0));
+        window.scrollTo(0, 0);
+        history.replace('work/jrello');
       }
     };
     return () => {
       window.onpagehide = null;
     };
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [pathname]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
