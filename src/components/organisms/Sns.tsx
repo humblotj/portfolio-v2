cced59a2a712798f979e9426c311aa7a705cf9c4
humblotj <jeanhumblot.dev@gmail.com>
2021-10-24 00:12:38 +0900

    Clean up sns

--- src/components/organisms/Sns.tsx
+++ src/components/organisms/Sns.tsx
@@ -2,7 +2,8 @@ import './Sns.scss';
 import { ReactComponent as GitHubIcon } from '../../assets/icons/github.svg';
 import { ReactComponent as LinkedInIcon } from '../../assets/icons/linkedin.svg';
 import { ReactComponent as CodepenIcon } from '../../assets/icons/codepen.svg';
-import { codepen, github, linkedin } from '../../utils/utils';
+import { ReactComponent as MailIcon } from '../../assets/icons/mail.svg';
+import { codepen, github, linkedin, mail } from '../../utils/utils';
 
 const Sns: React.FC<{}> = () => (
   <ul className="sns">
@@ -16,9 +17,14 @@ const Sns: React.FC<{}> = () => (
         <GitHubIcon />
       </a>
     </li>
-    <li>
+    <li className="codepen">
       <a target="_blank" rel="noreferrer" href={codepen} aria-label="Codepen">
         <CodepenIcon />
+      </a>
+    </li>
+    <li>
+      <a href={'mailto:' + mail} aria-label="Mail">
+        <MailIcon />
       </a>
     </li>
   </ul>
